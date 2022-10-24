f3421569a89a34fd738681cc3d330bc2c7290536
VrublevskyiO <vrublevskyi.o@gmail.com>
2022-10-24 19:53:04 +0300

    Matrix commit
    Added serialisation, exceptions handling, logging

--- Matrix-Polynom/Matrix/Matrix.cs
+++ Matrix-Polynom/Matrix/Matrix.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Serilog;
 namespace Matrix;
 
+[Serializable]
 public class Matrix
 {
+   
     private double[,] InternalMatrix { get; set; }
 
     public int Row { get; set; }
@@ -33,66 +39,94 @@ public class Matrix
     public static Matrix operator - (Matrix arr_1, Matrix arr_2)
     {
         Matrix res = new Matrix();
-        res = arr_1.Subtraction(arr_2);
+        res = arr_1.Subtract(arr_2);
         return res;
     }
     
     public static Matrix operator * (Matrix arr_1, Matrix arr_2)
     {
         Matrix res = new Matrix();
-        res = arr_1.Multiplication(arr_2);
+        res = arr_1.Multiply(arr_2);
         return res;
     }
     
     public void GetMatrixFromConsole(string name)
     {
-
-        Console.Write("Rows: {0}: ", name);
-        var row = int.Parse(Console.ReadLine());
-        Console.Write("Columns: {0}: ", name);
-        var column = int.Parse(Console.ReadLine());
-
-        Row = row;
-        Column = column;
-        //var matrix = new double[n, m];
-        InternalMatrix = new double[row, column];
-        for (var i = 0; i < row; i++)
+        try
         {
-            for (var j = 0; j < column; j++)
+            Log.Information("Method GetMatrixFromConsole was started");
+            
+            Console.Write("Rows: {0}: ", name);
+            var row = int.Parse(Console.ReadLine());
+            Console.Write("Columns: {0}: ", name);
+            var column = int.Parse(Console.ReadLine());
+
+            Row = row;
+            Column = column;
+            //var matrix = new double[n, m];
+            InternalMatrix = new double[row, column];
+            for (var i = 0; i < row; i++)
             {
-                Console.Write("{0}[{1},{2}] = ", name, i, j);
-                InternalMatrix[i, j] = int.Parse(Console.ReadLine());
+                for (var j = 0; j < column; j++)
+                {
+                    Console.Write("{0}[{1},{2}] = ", name, i, j);
+                    InternalMatrix[i, j] = int.Parse(Console.ReadLine());
+                }
             }
+            Log.Information(" GetMatrixFromConsole was successfully finished");
+        }
+        catch (System.ArrayTypeMismatchException ex)
+        {
+            Console.WriteLine("Catch error with element type. \nMore inf: {0}",ex);
+            Log.Debug("Catch error with element type");
+            Log.Error("Program was unsuccessfully finished");
+            throw;
         }
     }
     
     public void GetMatrixFromFile(string path)
     {
-        var input = File.ReadAllText(path);
-        Column = Int32.Parse(input.Substring(0, input.IndexOf(' ')));
-        Row = Int32.Parse(input.Substring(input.IndexOf(' '), input.IndexOf('\n')));
-        InternalMatrix = new double[Row, Column];
-        
-        int i = 0, j = 0, k = 0;
-        foreach (var row in input.Split('\n'))
+        try
         {
-            j = 0;
-            if (k == 0)
+            Log.Information("Method GetMatrixFromFile was started");
+            
+            var input = File.ReadAllText(path);
+            Column = Int32.Parse(input.Substring(0, input.IndexOf(' ')));
+            Row = Int32.Parse(input.Substring(input.IndexOf(' '), input.IndexOf('\n')));
+            InternalMatrix = new double[Row, Column];
+        
+            int i = 0, j = 0, k = 0;
+            foreach (var row in input.Split('\n'))
             {
-                k++; 
-                continue; 
+                j = 0;
+                if (k == 0)
+                {
+                    k++; 
+                    continue; 
+                }
+                foreach (var col in row.Trim().Split(' '))
+                {
+                    InternalMatrix[i, j] = int.Parse(col.Trim());
+                    j++;
+                }
+                i++;
             }
-            foreach (var col in row.Trim().Split(' '))
-            {
-                InternalMatrix[i, j] = int.Parse(col.Trim());
-                j++;
-            }
-            i++;
+           
+            Log.Information("GetMatrixFromFile was successfully finished");
+        }
+        catch (System.IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Catch error, massive has extra element(s). \nMore inf: {0}",ex);
+            Log.Debug("Catch error, massive has extra element(s)");
+            Log.Error("Program was unsuccessfully finished");
+            throw;
         }
     }
 
     public void PrintMatrix()
     {
+        Log.Information("Method PrintMatrix was started");
+        
         for (var i = 0; i < InternalMatrix.GetLength(0); i++)
         {
             for (var j = 0; j < InternalMatrix.GetLength(1); j++)
@@ -102,10 +136,14 @@ public class Matrix
             Console.WriteLine(" ");
         }
         Console.WriteLine(" ");
+        
+        Log.Information("PrintMatrix was successfully finished");
     }
     
     public void SaveMatrix(string path)
     {
+        Log.Information("Method SaveMatrix was started");
+        
         Console.WriteLine("Saved matrix:");
         for (var i = 0; i < InternalMatrix.GetLength(0); i++)
         {
@@ -117,14 +155,20 @@ public class Matrix
             File.AppendAllText(path, " \n");
         }
         File.AppendAllText(path, " \n");
+        
+        Log.Information("SaveMatrix was successfully finished");
     }
     
 
 
     public Matrix Add(Matrix extraArray)
     {
+        Log.Information("Method Add was started");
+        
         if (this.Row != extraArray.Row)
         {
+            Log.Error("Expected different dimensions of matrix");
+            Log.Error("Program was unsuccessfully finished");
             throw new Exception("Expected different dimensions of matrix");
         }
 
@@ -138,51 +182,115 @@ public class Matrix
             }
         }
 
+        Log.Information("Add was successfully finished");
+        
         return res;
     }
     
-    public Matrix Subtraction(Matrix extraArray)
+    public Matrix Subtract(Matrix extraArray)
     {
+        Log.Information("Method Subtract was started");
+        
         if (this.Row != extraArray.Row)
-        {
-            throw new Exception("Expected different dimensions of matrix");
-        }
-
-        Matrix res = new Matrix(this.InternalMatrix.GetLength(0), extraArray.Row);
-
-        for (var i = 0; i < InternalMatrix.GetLength(0); i++)
-        {
-            for (var j = 0; j < extraArray.Column; j++)
             {
-                res[i, j] = InternalMatrix[i, j] - extraArray[i, j];
+                Log.Error("Expected different dimensions of matrix");
+                Log.Error("Program was unsuccessfully finished");
+                throw new Exception("Expected different dimensions of matrix");
             }
-        }
 
-        return res;
+            Matrix res = new Matrix(Row, extraArray.Row);
+
+            for (var i = 0; i < InternalMatrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < extraArray.Column; j++)
+                {
+                    res[i, j] = InternalMatrix[i, j] - extraArray[i, j];
+                }
+            }
+            Log.Information("Subtract was successfully finished");
+            
+            return res;
     }
 
 
-    public Matrix Multiplication(Matrix extraArray)
+    public Matrix Multiply(Matrix extraArray)
     {
-        if (this.Row != extraArray.Column || this.Column != extraArray.Row)
+        try
         {
-            throw new Exception("Expected different dimensions of matrix");
-        }
+            Log.Information("Method Multiply was started");
+            
+            if (this.Row != extraArray.Column || this.Column != extraArray.Row)
+            {                
+                Log.Error("Expected different dimensions of matrix");
+                Log.Error("Program was unsuccessfully finished");
+                throw new Exception("Expected different dimensions of matrix");
+            }
 
-        Matrix res = new Matrix(InternalMatrix.GetLength(0), extraArray.Row);
+            Matrix res = new Matrix(InternalMatrix.GetLength(0), extraArray.Row);
 
-        for (var i = 0; i < InternalMatrix.GetLength(0); i++)
-        {
-            for (var j = 0; j < extraArray.Row; j++)
+            for (var i = 0; i < InternalMatrix.GetLength(0); i++)
             {
-                res[i, j] = 0;
-
-                for (var k = 0; k < InternalMatrix.GetLength(1); k++)
+                for (var j = 0; j < extraArray.Row; j++)
                 {
-                    res[i, j] += InternalMatrix[i, k] * extraArray[k, j];
+                    res[i, j] = 0;
+
+                    for (var k = 0; k < InternalMatrix.GetLength(1); k++)
+                    {
+                        res[i, j] += InternalMatrix[i, k] * extraArray[k, j];
+                    }
                 }
             }
+            Log.Information("Multiply was successfully finished");
+            
+            return res;
         }
-        return res;
+        
+        catch (System.ArithmeticException ex)
+        {
+            Console.WriteLine("Catch errors with arithmetic operations. \nMore inf: {0}",ex);
+            Log.Debug("Catch errors with arithmetic operations");
+            Log.Error("Program was unsuccessfully finished");
+            throw;
+        }
+
+    }
+    
+    public void SaveSerializedMatrix(string path)
+    {
+        Log.Information("Method SaveSerializedMatrix was started");
+        
+        Console.WriteLine("Saved serialized matrix:");
+
+        IFormatter formatter = new BinaryFormatter();
+        using (Stream stream = new FileStream(path, FileMode.Create))
+        {
+            formatter.Serialize(stream, InternalMatrix);
+        }
+        
+        Log.Information("SaveSerializedMatrix was successfully finished");
+    }
+
+    public void OpenSerializedMatrix(string path)
+    {
+        Log.Information("Method OpenSerializedMatrix was started");
+        
+        IFormatter formatter = new BinaryFormatter();
+        using (Stream stream = new FileStream(path, FileMode.Open))
+        {
+            double[,] dp = (double[,])formatter.Deserialize(stream);
+            for (var i = 0; i < dp.GetLength(0); i++)
+            {
+                for (var j = 0; j < dp.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", dp[i, j]);
+                }
+
+                Console.WriteLine(" ");
+            }
+
+            Console.WriteLine(" ");
+        }
+        
+        Log.Information("OpenSerializedMatrix was successfully finished");
     }
 }
--- Matrix-Polynom/Matrix/Program.cs
+++ Matrix-Polynom/Matrix/Program.cs
@@ -3,18 +3,29 @@
 //предусмотрев возможность их выполнения, в противном случае должно генерироваться пользовательское
 //исключение.
 //Сохранить значение матрицы в файл (сериализация) и восстановить из файла.
+using Serilog;
 
 namespace Matrix;
 class Program
 {
     static void Main()
     {
+        
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log_matrix.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Log.Information("Start program");
+        
         string path_1 = "/Users/vrublevskyi.o/RiderProjects/Gratyky_1.1/Gratyky_1.1/Matrix-Polynom/Matrix/matrix_1.txt";
         string path_2 = "/Users/vrublevskyi.o/RiderProjects/Gratyky_1.1/Gratyky_1.1/Matrix-Polynom/Matrix/matrix_2.txt";
         string path_3 = "/Users/vrublevskyi.o/RiderProjects/Gratyky_1.1/Gratyky_1.1/Matrix-Polynom/Matrix/res_matrix.txt";
         
         Matrix matrixA = new Matrix(); 
         Matrix matrixB = new Matrix();
+        
         //matrixA.GetMatrixFromConsole("matrixA");
         //matrixA.PrintMatrix();
         
@@ -33,15 +44,17 @@ class Program
         res = matrixA * matrixB;
         res.PrintMatrix();
         
-        res.SaveMatrix(path_3);
+        //res.SaveMatrix(path_3);
 
         //File.WriteAllText(path, originalText);
         //File.AppendAllText(path_3,res.ToString());
         
+        //string fileText = File.ReadAllText(path_3);
+        //Console.WriteLine(fileText);
         
+        res.SaveSerializedMatrix(path_3);
+        res.OpenSerializedMatrix(path_3);
         
-        string fileText = File.ReadAllText(path_3);
-        Console.WriteLine(fileText);
         
         Console.ReadLine();
     }
